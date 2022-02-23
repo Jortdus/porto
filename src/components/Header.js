c3043dd96cdaad2014e4cc90336b21e7baa399cd
Jort Broekhof <jortdus@gmail.com>
2022-02-23 21:59:19 +0100

    Initial structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Header() {
+    return (
+        <header className="Header">
+            <h1>test</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Header from "./components/Header";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Header />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+reportWebVitals();
